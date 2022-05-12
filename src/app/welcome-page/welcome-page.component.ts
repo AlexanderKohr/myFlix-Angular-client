2b69382d4f654d2c73241fa4a5d398b311628539
AlexanderKohr <94537064+AlexanderKohr@users.noreply.github.com>
2022-05-12 10:28:36 +0200

    Create welcome-page.component.ts

---
+++ src/app/welcome-page/welcome-page.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { UserLoginFormComponent } from '../user-login-form/user-login-form.component';
+import { UserRegistrationFormComponent } from '../user-registration-form/user-registration-form.component';
+
+@Component({
+  selector: 'app-welcome-page',
+  templateUrl: './welcome-page.component.html',
+  styleUrls: ['./welcome-page.component.scss']
+})
+export class WelcomePageComponent implements OnInit {
+  constructor(public dialog: MatDialog) { }
+  ngOnInit(): void { }
+
+  /* This is the function that will open the 
+  dialog when the signup button is clicked */
+  openUserRegistrationDialog(): void {
+    this.dialog.open(UserRegistrationFormComponent, {
+      // Assigning the dialog a width
+      width: '480px'
+    });
+  }
+
+  /* This function will open the dialog 
+  when the login button is clicked */
+  openUserLoginDialog(): void {
+    this.dialog.open(UserLoginFormComponent, {
+      // Assiging the dialog a width
+      width: '480px'
+    });
+  }
+}
